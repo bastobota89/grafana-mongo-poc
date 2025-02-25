49589c7fad031f79b517fb4eece08c081d4f298e
Manjurul Hoque Rumi <rumi@impelitsolutions.com>
2025-02-25 16:20:10 +0600

    update port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const Record = require('./models/Record');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 // CORS configuration
 const corsOptions = {
